129076550f4591e31f97c4cf80ed2bfebee0ba15
Rui Carneiro <rui.motacarneiro@heig-vd.ch>
2022-11-17 11:15:37 +0100

    Message class

---
+++ Client/src/Message.java
@@ -0,0 +1,17 @@
+public class Message {
+    private final String header;
+    private final String body;
+
+    public Message(String header, String body) {
+        this.header = header;
+        this.body = body;
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public String getBody() {
+        return body;
+    }
+}
