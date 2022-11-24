1d68040a80a9f215a14fa7bc4b18a8559846c3b2
Rui Carneiro <rui.motacarneiro@heig-vd.ch>
2022-11-24 11:19:57 +0100

    Mail instances and Regex check

--- Client/src/Mail.java
+++ Client/src/Mail.java
@@ -1,4 +1,5 @@
 import java.util.LinkedList;
+import java.util.regex.Pattern;
 
 public class Mail {
     private final int id;
@@ -6,10 +7,18 @@ public class Mail {
 
     static LinkedList<Integer> ids;
     static LinkedList<String> mails;
+    static LinkedList<Mail> instances;
+
+    static String emailRegex =
+            "^[a-zA-Z0-9_+&*-]+(?:\\."+
+            "[a-zA-Z0-9_+&*-]+)*@" +
+            "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+            "A-Z]{2,7}$";
 
     static {
         ids = new LinkedList<>();
         mails = new LinkedList<>();
+        instances = new LinkedList<>();
     }
 
     public Mail(int id, String mail) {
@@ -19,10 +28,19 @@ public class Mail {
         if (mails.contains(mail)) {
             throw new IllegalArgumentException("This e-mail already exists!");
         }
+
+        // Mail check
+        Pattern pat = Pattern.compile(emailRegex);
+        if (!pat.matcher(mail).matches()) {
+            throw new IllegalArgumentException(mail + " is not a valid e-mail!");
+        }
+
+        // Valid mail
         ids.add(id);
         mails.add(mail);
         this.id = id;
         this.mail = mail;
+        instances.add(this);
     }
 
     public int getId() {
@@ -36,5 +54,14 @@ public class Mail {
     @Override
     public String toString() {
         return "Mail id : " + id + ", mail : " + mail;
+    }
+
+    public static Mail getInstance(int id) {
+        for (Mail m : instances) {
+            if (m.id == id) {
+                return m;
+            }
+        }
+        return null;
     }
 }
