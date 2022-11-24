80e83eb2344b10d1cf755227c5578c6ab368ee85
Rui Carneiro <rui.motacarneiro@heig-vd.ch>
2022-11-24 11:20:39 +0100

    Checks for validty and Links between Groups and Messages

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -3,11 +3,15 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.LinkedList;
+import java.util.Random;
+import java.util.random.RandomGenerator;
 
 public class Client {
     public static void main(String[] args) {
         try {
+
             /* Mails */
+
             BufferedReader is = new BufferedReader(new FileReader(".\\CONFIG\\mail.config"));
             LinkedList<Mail> mails = new LinkedList<>();
 
@@ -22,7 +26,12 @@ public class Client {
                 System.out.println(ioEx.getMessage());
             }
 
+            if (mails.size() < 3) {
+                throw new RuntimeException("There must be at least 3 mails to create a group!");
+            }
+
             /* Groups */
+
             is = new BufferedReader(new FileReader(".\\CONFIG\\group.config"));
             LinkedList<Group> groups = new LinkedList<>();
 
@@ -30,26 +39,37 @@ public class Client {
                 while(is.ready()) {
                     String s = is.readLine();
                     String[] ss = s.split(", ");
-                    Integer[] in = new Integer[ss.length - 1];
-                    for (int i = 0; i < in.length; ++i) {
-                        in[i] = Integer.parseInt(ss[i + 1]);
+                    LinkedList<Mail> receivers = new LinkedList<>();
+                    for (int i = 0; i < ss.length - 1; ++i) {
+                        Mail m = Mail.getInstance(Integer.parseInt(ss[i + 1]));
+                        if (m != null) {
+                            receivers.add(m);
+                        }
+                        else {
+                            throw new RuntimeException("There is no mail with id " + ss[i + 1]);
+                        }
                     }
-                    groups.add(new Group(Integer.parseInt(ss[0]), in));
+                    groups.add(new Group(Mail.getInstance(Integer.parseInt(ss[0])), receivers));
                 }
             }
             catch(IOException ioEx) {
                 System.out.println(ioEx.getMessage());
             }
 
+            if (groups.size() < 1) {
+                throw new RuntimeException("There must be at least 1 group for the Client to run correctly!");
+            }
+
             /* Messages */
+
             is = new BufferedReader(new FileReader(".\\CONFIG\\message.config"));
             LinkedList<Message> messages = new LinkedList<>();
 
             try {
                 int i = 0;
+                String header = "";
+                String message = "";
                 while(is.ready()) {
-                    String header = "";
-                    String message = "";
                     switch (i) {
                         case 0 -> {
                             header = is.readLine();
@@ -58,22 +78,35 @@ public class Client {
                         }
                         case 1 -> {
                             String s = is.readLine();
-                            if (s.equals("\n")) {
+                            if (s.isEmpty()) {
                                 i = 0;
                                 messages.add(new Message(header, message));
                             } else {
-                                message += s;
+                                message += s + "\n";
                             }
                         }
                         default ->
                                 throw new RuntimeException("i has been put to something else than 0 and 1 in message reading.");
                     }
                 }
+                if (!header.isEmpty() && !message.isEmpty()) {
+                    messages.add(new Message(header, message));
+                }
             }
             catch(IOException ioEx) {
                 System.out.println(ioEx.getMessage());
             }
-            System.out.println(messages);
+
+            if (messages.size() < 1) {
+                throw new RuntimeException("There must be at least 1 message for the Client to run correctly!");
+            }
+
+            /* Links */
+
+            Random r = new Random();
+            for (Group g : groups) {
+                g.setMessage(messages.get(r.nextInt(messages.size())));
+            }
         }
         catch(FileNotFoundException fnfEx) {
             System.out.println(fnfEx.getMessage());
