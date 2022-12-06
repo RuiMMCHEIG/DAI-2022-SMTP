5817b3410116785ceb4a8b25bfba7760bbccdd81
Rui Carneiro <rui.motacarneiro@heig-vd.ch>
2022-12-06 10:34:43 +0100

    Removed useless group methods

--- Client/src/Group.java
+++ Client/src/Group.java
@@ -4,8 +4,6 @@ public class Group {
     private final Mail sender;
     private final LinkedList<Mail> receivers;
 
-    private Message message;
-
     {
         receivers = new LinkedList<>();
     }
@@ -18,11 +16,6 @@ public class Group {
         this.receivers.addAll(receivers);
     }
 
-    public Group(Mail sender, LinkedList<Mail> receivers, Message message) {
-        this(sender, receivers);
-        setMessage(message);
-    }
-
     public Mail getSender() {
         return sender;
     }
@@ -31,18 +24,11 @@ public class Group {
         return (LinkedList<Mail>) receivers.clone();
     }
 
-    public void setMessage(Message message) {
-        this.message = message;
-    }
-
     @Override
     public String toString() {
         StringBuilder s = new StringBuilder("Group sender : " + sender.getMail() + "\nGroup receivers :");
         for (Mail m : receivers) {
             s.append("\n-").append(m.getMail());
-        }
-        if (message != null) {
-            s.append("\nMessage : ").append(message.getHeader()).append("\n").append(message.getBody());
         }
         return s.toString();
     }
